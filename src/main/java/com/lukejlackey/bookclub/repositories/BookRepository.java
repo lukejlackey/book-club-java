19823b30ed49f2f2763e146bbc774b33667a87fd
Luke Lackey <gnomecitygames@outlook.com>
2022-08-15 16:34:30 -0700

    fixed repos

--- src/main/java/com/lukejlackey/bookclub/repositories/BookRepository.java
+++ src/main/java/com/lukejlackey/bookclub/repositories/BookRepository.java
@@ -3,9 +3,11 @@ package com.lukejlackey.bookclub.repositories;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.lukejlackey.bookclub.models.Book;
 
+@Repository
 public interface BookRepository extends CrudRepository<Book, Long> {
 	
 	List<Book> findAll();
